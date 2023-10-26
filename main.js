555e07beb3d1987c86c34299067126a65a4d5f74
rachannzz <rachanphuyal15@gmail.com>
2023-10-27 09:29:26 +1100

    added buttons

---
+++ main.js
@@ -0,0 +1,42 @@
+function add(a,b) {     //addition
+   let sum=0;       
+
+   return sum=a+b;  
+
+}
+
+
+function subtract(a,b) {    //subtraction
+    let sub=0;
+
+    return sub=a-b;
+    
+}
+
+function multiply(a,b) {    //multiplication
+    
+    let mul=0;
+
+    return mul=a*b;
+
+}
+
+function divide(a,b) {      //division
+
+    let div=0;
+
+    return div=a/b;
+    
+}
+
+function operate(firstnumber,operator,secondnumber) {
+
+    
+}
+
+
+let firstnumber;
+let operator;
+let secondnumber;
+
+
